2546df4ed603483c904d4459175497cfe55bf46d
Ram <ramkumarceg@gmail.com>
2014-09-30 11:04:51 -0700

    wrap selected js with an anonymous function

--- selected.js
+++ selected.js
@@ -1,49 +1,52 @@
-var $;
+(function() {
+  var $;
 
-$ = jQuery;
+  $ = jQuery;
 
-$.fn.selected = function(options) {
-  var bubbleUpEvent, parent, selectAll, targetClass;
-  if (options == null) {
-    options = {};
-  }
-  bubbleUpEvent = options.bubbleUpEvent != null ? options.bubbleUpEvent : false;
-  parent = options.parent != null ? options.parent : null;
-  targetClass = options.targetClass != null ? options.targetClass : null;
-  selectAll = options.selectAll != null ? options.selectAll : true;
-  return this.click(function(event) {
-    var className, clickTarget, targetEl;
-    if (!((event.target === event.currentTarget) && bubbleUpEvent)) {
-      return;
+  $.fn.selected = function(options) {
+    var bubbleUpEvent, parent, selectAll, targetClass;
+    if (options == null) {
+      options = {};
     }
-    if ($(event.target).is(this)) {
-      clickTarget = event.target;
-    } else {
-      clickTarget = $(event.target).closest(this)[0];
-    }
-    if (clickTarget === event.currentTarget) {
-      if (targetClass != null) {
-        targetEl = "." + targetClass;
-        clickTarget = $(event.target).closest(targetEl);
-        className = targetClass;
+    bubbleUpEvent = options.bubbleUpEvent != null ? options.bubbleUpEvent : false;
+    parent = options.parent != null ? options.parent : null;
+    targetClass = options.targetClass != null ? options.targetClass : null;
+    selectAll = options.selectAll != null ? options.selectAll : true;
+    return this.click(function(event) {
+      var className, clickTarget, targetEl;
+      if (!((event.target === event.currentTarget) && bubbleUpEvent)) {
+        return;
+      }
+      if ($(event.target).is(this)) {
+        clickTarget = event.target;
       } else {
-        clickTarget = $(clickTarget);
-        className = clickTarget.attr("class").split(" ")[0];
+        clickTarget = $(event.target).closest(this)[0];
       }
-      if (parent != null) {
-        $("" + parent + " [class*=" + className + "]").not(clickTarget).removeClass("selected odd even");
-      } else if (selectAll) {
-        $("*[class*=" + className + "]").not(clickTarget).removeClass("selected odd even");
+      if (clickTarget === event.currentTarget) {
+        if (targetClass != null) {
+          targetEl = "." + targetClass;
+          clickTarget = $(event.target).closest(targetEl);
+          className = targetClass;
+        } else {
+          clickTarget = $(clickTarget);
+          className = clickTarget.attr("class").split(" ")[0];
+        }
+        if (parent != null) {
+          $("" + parent + " [class*=" + className + "]").not(clickTarget).removeClass("selected odd even");
+        } else if (selectAll) {
+          $("*[class*=" + className + "]").not(clickTarget).removeClass("selected odd even");
+        }
+        clickTarget.addClass("selected");
+        if (clickTarget.hasClass("odd")) {
+          clickTarget.addClass("even");
+          clickTarget.removeClass("odd");
+        } else {
+          clickTarget.addClass("odd");
+          clickTarget.removeClass("even");
+        }
       }
-      clickTarget.addClass("selected");
-      if (clickTarget.hasClass("odd")) {
-        clickTarget.addClass("even");
-        clickTarget.removeClass("odd");
-      } else {
-        clickTarget.addClass("odd");
-        clickTarget.removeClass("even");
-      }
-    }
-    return clickTarget.trigger("cssClassChanged");
-  });
-};
+      return clickTarget.trigger("cssClassChanged");
+    });
+  };
+
+}).call(this);
